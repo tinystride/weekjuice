50c8118249c42e6f2053593acf45d16119f67bed
Neil Renicker <neilrenicker@gmail.com>
2014-05-07 00:10:41 -0400

    wip: random post node logic

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-var express = require('express');
-var app = express();
+function postsCounter() {
+  request = require('request-json');
+  var client = request.newClient('https://glowing-fire-6569.firebaseio.com');
+
+  function postsCount() {
+    client.get('posts.json', function(err, res, body) {
+      if (!err) {
+        var result = body.length;
+        return result;
+      }
+    });
+  }
+
+  return postsCount();
+};
+
+console.log(postsCounter());
 
 var schedule = require('node-schedule');
 
 var rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = 1;
+// rule.dayOfWeek = 1;
+rule.second = [new schedule.Range(0, 59)];
 
 
 function randomNumber() {
-  var randomItem = Math.random();
+  var numberOfPosts = postsCounter();
+  var randomItem = Math.floor(Math.random()*numberOfPosts);
   return randomItem;
 };
 
