34900299dca6e9a10b15ba1c28f46fcbfe2d9655
Neil Renicker <neilrenicker@gmail.com>
2014-05-20 23:03:21 -0400

    wip: ajax call for random post from Firebase

--- assets/js/app/controllers/applicationController.js
+++ assets/js/app/controllers/applicationController.js
@@ -3,7 +3,24 @@ App.ApplicationController = Ember.ArrayController.extend({
 
   randomPost: function() {
     var itemsCount = this.get('length');
-    var randomItem = (Math.floor(Math.random()*itemsCount));
-    return this.objectAt(randomItem);
+
+    var randomItemNumber;
+
+    function retrieveRandomItemNumber() {
+      return $.ajax({
+        url: "https://glowing-fire-6569.firebaseio.com/currentPost.json",
+        dataType: 'json'
+      });
+    }
+
+    var setRandomItem = retrieveRandomItemNumber().done(function(result) {
+      var randomItemNumber = result;
+      // I think I just need to fix this "this"
+      // scoping error for the program to work.
+      return this.objectAt(randomItemNumber);
+    }).fail(function() {
+      console.log( "Oops, the promise failed." );
+    });
+
   }.property('length')
 });
--- assets/js/app/models/currentPost.js
+++
@@ -1,4 +0,0 @@
-App.currentPost = DS.Model.extend({
-  // var currentPost = store.find('currentPost')
-  // console.log(currentPost);
-})
