fa85549d8df9b7b32dde9f71b71e1ee65ab42064
Neil Renicker <neilrenicker@gmail.com>
2014-06-19 22:56:55 -0400

    wip: mostly working currentPost api

--- server.js
+++ server.js
@@ -18,15 +18,18 @@ var APP = {
         alert("Oops, no data here partner.");
       } else {
         var currentPostCreationDate = snapshot.val().creationDate;
-        console.log(currentPostCreationDate);
         var currentPostPrimaryContent = snapshot.val().primary_content;
-        console.log(currentPostPrimaryContent);
         var currentPostSourceNote = snapshot.val().sourceNote;
-        console.log(currentPostSourceNote);
         var currentPostSourceTitle = snapshot.val().sourceTitle;
-        console.log(currentPostSourceTitle);
         var currentPostSourceUrl = snapshot.val().sourceUrl;
-        console.log(currentPostSourceUrl);
+
+        APP.setCurrentPostData(
+          currentPostCreationDate,
+          currentPostPrimaryContent,
+          currentPostSourceNote,
+          currentPostSourceTitle,
+          currentPostSourceUrl
+        );
       }
     });
   },
@@ -48,6 +51,22 @@ var APP = {
     });
   },
 
+  setCurrentPostData: function(
+      currentPostCreationDate,
+      currentPostPrimaryContent,
+      currentPostSourceNote,
+      currentPostSourceTitle,
+      currentPostSourceUrl
+    ) {
+
+    var currentPostSetRef = new Firebase('https://glowing-fire-6569.firebaseio.com/currentPost');
+    currentPostSetRef.child('creationDate').set(currentPostCreationDate);
+    currentPostSetRef.child('primary_content').set(currentPostPrimaryContent);
+    currentPostSetRef.child('sourceNote').set(currentPostSourceNote);
+    currentPostSetRef.child('sourceTitle').set(currentPostSourceTitle);
+    currentPostSetRef.child('sourceUrl').set(currentPostSourceUrl);
+  },
+
   // set up node-schedule job:
 
   runSchedule: function() {
@@ -56,19 +75,14 @@ var APP = {
     rule.minute = [new schedule.Range(0, 59)];
 
     // initiate node-schedule job to update
-
     var job = schedule.scheduleJob(rule, function() {
       APP.getCurrentPost();
-      // console.log('currentPostCreationDate: ' + currentPostCreationDate);
-
-      // var currentPostSetRef = new Firebase('https://glowing-fire-6569.firebaseio.com/currentPost');
-      // currentPostSetRef.child('creationDate').set(currentPostCreationDate);
-      // currentPostSetRef.child('primary_content').set(currentPostPrimaryContent);
-      // currentPostSetRef.child('sourceNote').set(currentPostSourceNote);
-      // currentPostSetRef.child('sourceTitle').set(currentPostSourceTitle);
-      // currentPostSetRef.child('sourceUrl').set(currentPostSourceUrl);
     });
+  },
+
+  init: function(){
+    APP.runSchedule();
   }
 };
 
-APP.runSchedule();
+APP.init();
